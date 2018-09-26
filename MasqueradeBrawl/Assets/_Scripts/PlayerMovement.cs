d0cf2e6eb6b0a443e1daf4532680e28b2fbc593c
LoganDuffy702 <32749000+LoganDuffy702@users.noreply.github.com>
2018-09-25 19:47:05 -0600

    Testing Assests

---
+++ MasqueradeBrawl/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject Character;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    public float speed;
+    public float Maxspeed;
+    //bool Flipped;
+	
+
+
+	void Start () {
+        rb = Character.GetComponent<Rigidbody2D>();
+        sr = Character.GetComponent<SpriteRenderer>();
+	}
+	void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            //rb.velocity = (new Vector2(speed, rb.velocity.y));
+            rb.AddForce(new Vector2(speed, rb.velocity.y));
+            sr.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            //rb.velocity = (new Vector2(-speed, rb.velocity.y));
+            rb.AddForce(new Vector2(-speed, rb.velocity.y));
+            sr.flipX = false;
+        }
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, Maxspeed);
+
+        //if (Flipped == true)
+        //{
+        //    WSTrans.transform.localPosition = new Vector2(0.007f, -0.32f);
+        //    WSsr.flipY = true;
+
+        //}
+        //else if (Flipped == false)
+        //{
+        //    WSTrans.transform.localPosition = new Vector2(0.007f, 0.32f);
+        //    WSsr.flipY = false;
+        //}
+
+    }
+	
+	
+}
---
+++ MasqueradeBrawl/Assets/_Scripts/Portal_Factory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal_Factory : MonoBehaviour {
+
+    public List<GameObject> Portals = new List<GameObject>();
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
