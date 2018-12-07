71a50af583d107a482e6d9b7c47f48a991a279cf
LoganDuffy702 <32749000+LoganDuffy702@users.noreply.github.com>
2018-12-07 00:05:26 -0700

    puuuuuusssshhhhh

--- MasqueradeBrawl/Assets/_Scripts/NextLevel.cs
+++ MasqueradeBrawl/Assets/_Scripts/NextLevel.cs
@@ -8,21 +8,22 @@ public class NextLevel : MonoBehaviour {
 
     public bool CanSelectLevel = false;
     public List<GameObject> Levels;
-    public bool Nextbutton;
+    public bool Fadein;
     public string LastPickerA;
     public GameObject Lamp1;
+    public GameObject ControllsScreen;
     public GameObject Lamp2;
+    
     public string LastPickerHor;
-    int currentNum = 0;
+    
    
     public void Start()
     {
         Lamp1.GetComponent<SpriteRenderer>().color = Color.gray;
         Lamp2.GetComponent<SpriteRenderer>().color = Color.gray;
-        for (int i = 1; i < Levels.Count; i++)
-        {
-            Levels[i].SetActive(false);
-        }
+        Levels[0].SetActive(true);
+        Levels[1].SetActive(true);
+        Fadein = false;
     }
 
     public void ActivateLevel()
@@ -48,61 +49,38 @@ public class NextLevel : MonoBehaviour {
     {
         if (CanSelectLevel == true)
         {
-            if (Input.GetButtonDown(LastPickerA))//Player Selected
+
+            if (Input.GetButtonDown("Player1_Start") ||Input.GetButtonDown("Player2_Start") ||
+                Input.GetButtonDown("Player3_Start") || Input.GetButtonDown("Player4_Start"))
             {
-                if (Levels[currentNum].name == "StaticButtonActive")
-                {
-                    Debug.Log("Load Static Level");
-                    StartCoroutine(LoadLevel(2));
-                }
-                else if (Levels[currentNum].name == "FallingButtonActive")
-                {
-                    Debug.Log("Load Falling Level");
-                    StartCoroutine(LoadLevel(3));
-                }
+                Levels[0].SetActive(false);
+                StartCoroutine(LoadLevel(2));
             }
-
-            if (Input.GetAxis(LastPickerHor) == 0)
+            else if (Input.GetButtonDown("Player1_Back") || Input.GetButtonDown("Player2_Back") || Input.GetButtonDown("Player3_Back") || Input.GetButtonDown("Player4_Back"))
             {
-                Nextbutton = true;
+                Levels[1].SetActive(false);
+                StartCoroutine(LoadLevel(3));
             }
+        }
 
-            if (Input.GetAxis(LastPickerHor) > 0.2 && Nextbutton == true)
-            {
-                Nextbutton = false;
-                currentNum += 1;
-              
-                //---------------------------------------------
-                if (currentNum >= Levels.Count)
-                {
-                    currentNum = 0;//change to 1? try to skip unselected screen.
-                }
-                else if (currentNum < 0)
-                {
-                    currentNum = Levels.Count;
-                }
+        if (Fadein == true)
+        {
 
-                for (int x = 0; x < currentNum; x++)
-                {
-                    Levels[x].SetActive(false);
-                }
-                Levels[currentNum].SetActive(true);
-                for (int x = currentNum + 1; x <= Levels.Count - 1; x++)
-                {
-                    Levels[x].SetActive(false);
-                }
-                //Movesound.pitch += .2f;
-                //Movesound.Play();
-
-            }
-
+            ControllsScreen.GetComponent<SpriteRenderer>().color = new Color(255,255,255, 1);
+        }
+        else if (Fadein == false)
+        {
+            ControllsScreen.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255,0);
         }
            
     }
     public IEnumerator LoadLevel(int indexNum)
     {
-        Debug.Log("Loading Level");
-        yield return new WaitForSeconds(1f);
+        //Debug.Log("Loading Level");
+        yield return new WaitForSeconds(2f);
+        Fadein = true;
+        yield return new WaitForSeconds(7f);
+        Fadein = false;
         SceneManager.LoadScene(indexNum);
     }
 }
