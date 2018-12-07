50b5838da400baae0d09d3c9b20e94f062c08aaa
LoganDuffy702 <32749000+LoganDuffy702@users.noreply.github.com>
2018-12-06 17:43:33 -0700

    asdzxfcgjmkl

---
+++ MasqueradeBrawl/Assets/boucepad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boucepad : MonoBehaviour {
+
+    public GameObject Particals;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Penguin") || other.gameObject.CompareTag("MoonMan") ||
+            other.gameObject.CompareTag("ButtLady") || other.gameObject.CompareTag("Foxy"))
+        {
+            var PP = Instantiate(Particals);
+            PP.transform.position = other.transform.position;
+            PP.GetComponent<ParticleSystem>().Play();
+            Destroy(PP, 1f);
+        }
+    }
+}
